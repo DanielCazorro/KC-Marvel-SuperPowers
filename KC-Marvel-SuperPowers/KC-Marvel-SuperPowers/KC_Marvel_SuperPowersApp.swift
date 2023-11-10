c936dc590fab9071d345374227a71a3089eb025a
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-10 09:48:20 +0100

    Initial commit

---
+++ KC-Marvel-SuperPowers/KC-Marvel-SuperPowers/KC_Marvel_SuperPowersApp.swift
@@ -0,0 +1,20 @@
+//
+//  KC_Marvel_SuperPowersApp.swift
+//  KC-Marvel-SuperPowers
+//
+//  Created by Daniel Cazorro Frías on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct KC_Marvel_SuperPowersApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
