3d047a39bdb1a877f61496c9ccbfeca300c8784c
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-18 21:37:43 +0100

    Main View details

--- KC-Marvel-SuperPowers/KC-Marvel-SuperPowers/UI/View/MainView.swift
+++ KC-Marvel-SuperPowers/KC-Marvel-SuperPowers/UI/View/MainView.swift
@@ -12,27 +12,34 @@ struct MainView: View {
     @State private var filter: String = ""
     
     var body: some View {
-        
-        NavigationStack{
-            List{
+        NavigationView {
+            List {
                 if let characters = mainViewModel.characters {
                     ForEach(characters) { character in
                         NavigationLink {
                             MainDetailView(character: character)
                         } label: {
                             MainRowView(character: character)
-                                .frame(height: 200)
+                                .frame(height: 180)
+                                .padding(.vertical, 8)
+                                .background(Color.clear)
+                                .cornerRadius(25)
+                                .shadow(color: Color.black.opacity(0.2), radius: 8, x: 0, y: 4)
+                                .transition(.move(edge: .leading))
                         }
                     }
                 }
             }
+            .listStyle(PlainListStyle())
+            .padding(.horizontal)
+            .background(Color.clear)
             .navigationTitle("Marvel")
         }
         .searchable(text: $filter,
                     placement: .navigationBarDrawer(displayMode: .automatic),
                     prompt: "Search Characters")
         .onChange(of: filter) { oldValue, newValue in
-            //Ha cambiado el filtro de busqueda
+            //Ha cambiado el filtro de búsqueda
             mainViewModel.getCharacters()
         }
     }
