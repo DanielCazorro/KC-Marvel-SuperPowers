e6457a77a0e4315c7cfc31c91bd72c2a9ba1fa4e
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-18 20:10:22 +0100

    Trying Allo Images from info.plist

--- KC-Marvel-SuperPowers/KC-Marvel-SuperPowers/UI/View/MainRowView.swift
+++ KC-Marvel-SuperPowers/KC-Marvel-SuperPowers/UI/View/MainRowView.swift
@@ -8,28 +8,34 @@
 import SwiftUI
 
 struct MainRowView: View {
-    let character: Character
+    var character: Character
     
     var body: some View {
-        VStack {
-            // Otros detalles del personaje...
-            if let imageURL = URL(string: "\(character.thumbnail.path).\(character.thumbnail.thumbnailExtension ?? "jpg")") {
-                AsyncImage(url: imageURL) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100)
-                } placeholder: {
-                    // Placeholder mientras la imagen se carga
-                    ProgressView()
-                }
-            } else {
-                Text("URL de imagen incorrecta")
+        
+        ZStack {
+            
+            AsyncImage(url: URL(string: character.thumbnail.path)) { photo in
+                photo
+                    .resizable()
+                    .cornerRadius(20)
+                    .opacity(0.8)
+                
+            } placeholder: {
+                ProgressView()
             }
-        }
-        .onAppear {
-            if let imageURL = URL(string: "\(character.thumbnail.path).\(character.thumbnail.thumbnailExtension ?? "jpg")") {
-                print("URL de imagen para \(character.name): \(imageURL)")
+            
+            VStack(alignment: .leading) {
+                // NAme
+                HStack{
+                    Text(character.name)
+                        .font(.title2)
+                        .foregroundStyle(.black)
+                        .bold()
+                        .padding([.top, .leading], 20)
+                    Spacer()
+                }
+                Spacer()
+                
             }
         }
     }
